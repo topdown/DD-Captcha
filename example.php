787e121c01d24eccd8ce20e16a779528aaa15529
Jeff Behnke <jeff@valid-webs.com>
2012-10-11 22:50:55 -0500

    Just adding the base html for the example file

--- example.php
+++ example.php
@@ -1,3 +1,17 @@
 <?php
 
 // This will hold examples once the libs are wrote.
+
+?>
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title>DD Captcha example</title>
+
+
+</head>
+<body>
+	Hello World
+</body>
+</html>
